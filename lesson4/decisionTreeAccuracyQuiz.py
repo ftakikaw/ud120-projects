9c7471323ed02f39708fca91aa92adbaababc9fe
ftakikaw <fkt_info@yahoo.com.br>
2017-05-29 23:36:16 -0300

    Atualizaçao repositorio lesson3 e lesson4

---
+++ lesson4/decisionTreeAccuracyQuiz.py
@@ -0,0 +1,35 @@
+import sys
+from class_vis import prettyPicture
+from prep_terrain_data import makeTerrainData
+
+import numpy as np
+import pylab as pl
+
+features_train, labels_train, features_test, labels_test = makeTerrainData()
+
+
+
+#################################################################################
+
+
+########################## DECISION TREE #################################
+
+
+#### your code goes here
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier()
+    
+clf = clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+acc = accuracy_score(pred, labels_test)
+
+print acc
+
+def submitAccuracies():
+    
+    return {"acc":round(acc,3)}
+
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -9,7 +9,7 @@
 """
     
 import sys
-from time import time
+import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -24,7 +24,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
 
+#clf = SVC(kernel="linear")
+clf = SVC(C=10000, kernel="rbf")
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+print "Inicio treinamento: ", time.asctime( time.localtime(time.time()) )
+clf.fit(features_train, labels_train)
+print "Termino treinamento: ", time.asctime( time.localtime(time.time()) )
+
+print "Inicio Teste: ", time.asctime( time.localtime(time.time()) )
+pred = clf.predict(features_test)
+print "Termino Teste: ", time.asctime( time.localtime(time.time()) )
+
+print "no. of Chris predicted emails:", sum(pred)
+print "no. of Sara predicted emails:", len(pred)-sum(pred)
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+print acc
 #########################################################
 
 
