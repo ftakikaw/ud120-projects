344e31d0d9c0410e6ab524a10b64f85fa9ba8c90
ftakikaw <fkt_info@yahoo.com.br>
2017-05-31 01:40:10 -0300

    Alteração percentil

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -69,7 +69,7 @@ def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/ema
     ### selector.transform
     ### features_train_transformed: tuple[15820,3785]
     ### features_test_transformed: tuple[1758,3785]    
-    #selector = SelectPercentile(f_classif, percentile=0)
+    #selector = SelectPercentile(f_classif, percentile=1)
     selector = SelectPercentile(f_classif, percentile=10)
     
     selector.fit(features_train_transformed, labels_train)
