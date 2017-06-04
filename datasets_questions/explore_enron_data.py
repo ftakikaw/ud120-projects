fe60b5592b5d6d2c5b2e219d3b31159841d5cabe
ftakikaw <fkt_info@yahoo.com.br>
2017-06-04 02:28:19 -0300

    upload lesson 7

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -24,18 +24,26 @@ number_total = len(enron_data.items())
 number_poi = 0
 number_salario_nan = 0
 number_email_nan = 0
+number_total_payment_nan = 0
 
 for key, value in enron_data.items():
     
     if(value['poi']):
         number_poi = number_poi + 1
+
     if(str(value['salary']) == 'NaN'):
         number_salario_nan = number_salario_nan + 1
         
     if(str(value['email_address']) == 'NaN'):
         number_email_nan = number_email_nan + 1
+    
+    if(str(value['total_payments']) == 'NaN'):
+        number_total_payment_nan = number_total_payment_nan + 1
+
 
 print "qde total: ", number_total 
 print "qde poi: ", number_poi
 print "qde salario informado: ", number_total - number_salario_nan
 print "qde email informado: ", number_total - number_email_nan
+print "qde total payment NaN", number_total_payment_nan
+print "% total payment NaN: ", (number_total_payment_nan / (number_total + 0.0))
